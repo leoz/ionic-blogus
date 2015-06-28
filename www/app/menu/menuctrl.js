c3461b74746bcbb8be80cf5c986283a6e2777c1b
leoz2011 <leoz@yahoo.com>
2015-06-27 23:15:31 -0400

    Add bookmark count

--- www/app/menu/menuctrl.js
+++ www/app/menu/menuctrl.js
@@ -3,7 +3,8 @@ angular.module('MenuCtrl', [])
 
 .controller('MenuController', function($scope) {
     $scope.main = {
-        show_list: 'bookmarks'
+        show_list: 'bookmarks',
+        show_delete: false
     };
 });
 
